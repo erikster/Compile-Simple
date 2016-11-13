ef17f3c92fb68be97791da8c5d71e9fb3b0fcb3a
Erik Steringer <deveriks@live.com>
2016-11-13 14:02:30 -0800

    initial stuff, outlining plans

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ *  Main class. Takes an input file, tokenizes it, parses it, then evaluates it.
+ *
+ *  @author Erik Steringer
+ */
+ public class Main {
+    public static void main(String[] args) {
+        System.out.println("Placeholder");
+    }
+ }
