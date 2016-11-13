20a0fc4dc6ea2aeab49e4a5db25900fea00e09ab
Erik Steringer <deveriks@live.com>
2016-11-13 14:29:40 -0800

    Starting ST work.

--- SyntaxNode.java
+++ SyntaxNode.java
@@ -1,7 +1,12 @@
-
+/*  Tree stuff. Needs nodes, leaves.
+ *  Each node has a value, represented as a string.
+ *
+ */
 
 public class SyntaxNode {
-
+    public SyntaxNode left;
+    public SyntaxNode right;
+    public String value;
 }
 
 public enum NodeType {
