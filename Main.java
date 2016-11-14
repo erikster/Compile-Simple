0d48048227b7128006039b5103baeb56a0f9324f
Erik Steringer <deveriks@live.com>
2016-11-13 18:11:48 -0800

    first actual output

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Queue;
 
 /**
  *  Main class. Takes an input file, tokenizes it, parses it, then evaluates it.
@@ -7,6 +8,11 @@ import java.io.*;
  */
  public class Main {
     public static void main(String[] args) {
-        System.out.println("Placeholder");
+        Tokenizer tr = new Tokenizer();
+        tr.buildTokensFromString("2 + ( 1 + 2 )");
+        Queue<Token> queue = tr.getTokens();
+        for (Token t : queue) {
+            System.out.println(t);
+        }
     }
  }
--- Token.java
+++ Token.java
@@ -10,5 +10,28 @@ public class Token {
         this.type  = type;
         this.value = value;
     }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        switch (type) {
+            case VALUE:
+                sb.append("VALUE");
+                break;
+            case OPENPARENS:
+                sb.append("OPENPARENS");
+                break;
+            case CLOSEPARENS:
+                sb.append("CLOSEPARENS");
+                break;
+            case OPERATOR:
+                sb.append("OPERATOR");
+                break;
+        }
+        sb.append(": \"");
+        sb.append(value);
+        sb.append("\"");
+        return sb.toString();
+    }
 }
 
